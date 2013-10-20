ad6ca0588d409910d0a40bb67d90576db846ca19
liyao <love.liyao@gmail.com>
2013-10-20 23:48:21 +0800

    init

---
+++ scripts/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	var pepe = $.fn.fullpage({
+		slidesColor: ['#004061', '#1abc9c','#65934A','#232121'],
+		anchors: ['home', 'services', 'about', 'contact'],
+		menu: '#menu'
+	});
+	
+});
